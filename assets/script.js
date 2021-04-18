7b97c108d58f3c012799d494ffbd29daa014288a
jocelynrem <jocelynrem@gmail.com>
2021-04-18 15:42:35 -0400

    basic quiz part is working

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,112 @@
-var startButton = document.getElementById('start-btn')
-var questionContainerEl = document.getElementById('question-container')
+var startButton = document.getElementById("start-btn");
+var nextButton = document.getElementById("next-btn");
+var questionContainerEl = document.getElementById("question-container");
+var questionEl = document.getElementById("question");
+var answerBtnEl = document.getElementById("answer-buttons");
+var shuffledQuestions, currentQuestionIndex;
+var countRightAnswers = 0;
 
-var shuffledQuestions, currentQuestionIndex
-
-startButton.addEventListener('click', startGame)
+startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", () => {
+  currentQuestionIndex++;
+  setNextQuestion();
+});
 
 function startGame() {
-    startButton.classList.add('hide')
-    shuffledQuestions
-    questionContainerEl.classList.remove('hide')
-    setNextQuestion()    
+  countRightAnswers = 0;
+  startButton.classList.add("hide");
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
+  questionContainerEl.classList.remove("hide");
+  setNextQuestion();
 }
 
 function setNextQuestion() {
-
+  resetState();
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
 }
 
-function selectAnswer() {
+function showQuestion(question) {
+  questionEl.innerText = question.question;
+  question.answers.forEach((answer) => {
+    var button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add("btn");
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+    answerBtnEl.appendChild(button);
+  });
+}
 
+function resetState() {
+  clearStatusClass(document.body);
+  nextButton.classList.add("hide");
+  while (answerBtnEl.firstChild) {
+    answerBtnEl.removeChild(answerBtnEl.firstChild);
+  }
+}
+
+function selectAnswer(e) {
+  var selectedButton = e.target;
+  var correct = selectedButton.dataset.correct;
+  setStatusClass(document.body, correct);
+  Array.from(answerBtnEl.children).forEach((button) => {
+    setStatusClass(button, button.dataset.correct);
+  });
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove("hide");
+  } else {
+    startButton.innerText = "Restart";
+    startButton.classList.remove("hide");
+  }
+  if(selectedButton.dataset = correct) {
+      countRightAnswers++;
+  }
+  document.getElementById('right-answers').innerText = "Correct Answers: " + countRightAnswers;
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element);
+  if (correct) {
+    element.classList.add("correct");
+  } else {
+    element.classList.add("wrong");
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove("correct");
+  element.classList.remove("wrong");
 }
 
 var questions = [
-    {
-        question: 'What is 2 + 2',
-        answers: [
-            { text: '4', correct: true },
-            { text: '22', correct: false }
-        ]
-    }
-]
+  {
+    question: "What is 2 + 2?",
+    answers: [
+      { text: "4", correct: true },
+      { text: "22", correct: false },
+      { text: "5", correct: false },
+      { text: "2", correct: false },
+    ],
+  },
+  {
+    question: "What is 2 + 5?",
+    answers: [
+      { text: "3", correct: false },
+      { text: "7", correct: true },
+      { text: "9", correct: false },
+      { text: "6", correct: false },
+    ],
+  },
+  {
+    question: "What is 4 + 2?",
+    answers: [
+      { text: "3", correct: false },
+      { text: "7", correct: false },
+      { text: "9", correct: false },
+      { text: "6", correct: true },
+    ],
+  },
+];
